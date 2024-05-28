a2861df32e8458be67537d0ae2e7bf9b2dd1157c
agungdhewe <agung_dhewe@yahoo.com>
2024-05-28 16:51:34 +0700

    bug fixing

--- dwpath.go
+++ dwpath.go
@@ -8,43 +8,45 @@ import (
 
 func IsPathExists(path string) (bool, fs.FileInfo, error) {
 	fileinfo, err := os.Stat(path)
-	if err == nil {
-		return true, fileinfo, err
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil, nil
+		} else {
+			return false, nil, err
+		}
 	}
-	if os.IsNotExist(err) {
-		return false, nil, fmt.Errorf("path '%s' tidak ditemukan", path)
-	}
-	return false, nil, err
+	return true, fileinfo, nil
 }
 
 func IsDirectoryExists(dir string) (bool, error) {
 	exists, fileinfo, err := IsPathExists(dir)
-	if !exists {
-		if err != nil {
-			return false, err
-		} else {
-			return false, nil
-		}
-	} else {
-		if !fileinfo.IsDir() {
-			return false, fmt.Errorf("path '%s' bukan direktori", dir)
-		}
+	if err != nil {
+		return false, err
 	}
+
+	if !exists {
+		return false, nil
+	}
+
+	if !fileinfo.IsDir() {
+		return false, fmt.Errorf("path '%s' bukan direktori", dir)
+	}
+
 	return true, nil
 }
 
 func IsFileExists(path string) (bool, fs.FileInfo, error) {
 	exists, fileinfo, err := IsPathExists(path)
 	if err != nil {
-		return false, fileinfo, err
+		return false, nil, err
+	}
+
+	if !exists {
+		return false, nil, nil
 	}
 
 	if fileinfo.IsDir() {
 		return false, fileinfo, fmt.Errorf("path '%s' bukan file", path)
-	}
-
-	if !exists {
-		return false, nil, fmt.Errorf("file '%s' tidak ditemukan", path)
 	}
 
 	return true, fileinfo, nil
